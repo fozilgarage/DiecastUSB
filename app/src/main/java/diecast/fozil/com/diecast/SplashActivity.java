c03675a7852dd4325c4af173fe12c136965e0db3
eduardobenitez <asidefocil@gmail.com>
2018-05-06 12:46:26 -0500

    Refactor

--- app/src/main/java/diecast/fozil/com/diecast/SplashActivity.java
+++ app/src/main/java/diecast/fozil/com/diecast/SplashActivity.java
@@ -1,20 +1,12 @@
 package diecast.fozil.com.diecast;
 
-import android.content.Context;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
-import databases.DataBaseManager;
-import databases.DataBaseManagerUpdates;
-
 public class SplashActivity extends AppCompatActivity {
-
-    private static int SPLASH_TIME = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +14,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_splash);
-
+        int SPLASH_TIME = 1500;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
